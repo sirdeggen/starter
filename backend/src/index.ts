b7cf0787617a7b14995adeb62b0aa968630544ef
Deggen <d.kellenschwiler@bsvassociation.org>
2025-10-17 07:50:14 -0500

    set up standard front and back ends with recent deps

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.json({ hello: 'world' })
+})
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`)
+})
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App (): React.ReactNode {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
